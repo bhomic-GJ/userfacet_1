d59eff584a032086c7d716ff928e00cdde696143
Glitch (popsearchgj) <none>
2022-07-29 06:57:58 +0000

    🎱🪐 Updated with Glitch

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.jsx";
+import { HelmetProvider } from 'react-helmet-async';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
